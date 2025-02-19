a6c1a84f877172165fd679f3fab34c46a7c6a195
dir <121112355+Dirtoussef@users.noreply.github.com>
2025-02-19 16:27:28 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-
+#
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
 # Display the JSON response properly
