93bb5dfd301dc8a1b1f4759c27125b397889feaf
dir <121112355+Dirtoussef@users.noreply.github.com>
2025-02-19 16:12:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import requests
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 cnx= st.connection("snowflake")
 session = cnx.session()
 # Write directly to the app
