8c5382915fe6dbe8b65d109eb6c9d8509a936360
dir <121112355+Dirtoussef@users.noreply.github.com>
2025-02-19 16:41:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import requests
 
 # Fetching data from SmoothieFroot API
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
 # Establishing Snowflake connection
 cnx = st.connection("snowflake")
