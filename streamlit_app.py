a59d0f8e2a9f7debdf01309e80e71948ea65a4ab
dir <121112355+Dirtoussef@users.noreply.github.com>
2025-02-20 12:10:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,7 @@ st.title("Customize your Smoothie 🥤")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe,use_container_width=True)
 # st.stop ()
-pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+
 # Adding text input for name on order
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be', name_on_order)
@@ -37,13 +35,13 @@ if ingredients_list:
     ingredients_string = ' '
     for fruit_chosen in ingredients_list:
       ingredients_string+= fruit_chosen + ' '
-      search_on=pd_df.loc[pd_df['FRUIT_NAME']== fruit_chosen, 'SEARCH_ON'].iloc[0]
+      
       # st.write('The search value for ', fruit_chosen, ' is ' , search_on, ',')
-      st.subheader(fruit_chosen + ' Nutrition Information ')
-      #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen )
-      #sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True) 
-      fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on )
-      fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
+      
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen )
+      f_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True) 
+      #fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on )
+      #fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     
        
     # Prepare SQL insert statement
